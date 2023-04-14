caeddb4d24665ef8f6fefb39bf72dbd648d4c7d0
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-04-14 16:43:54 +0100

    uncommon-goods beginnings

---
+++ src/main/java/com/damjan/uncommongoods/UncommongoodsApplication.java
@@ -0,0 +1,13 @@
+package com.damjan.uncommongoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncommongoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncommongoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damjan/uncommongoods/UncommongoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damjan.uncommongoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncommongoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
